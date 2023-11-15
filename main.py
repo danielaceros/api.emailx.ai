7c141d100bc80d9f22061ae20318d4497ddad9fa
Daniel Acero Sagredo <danielacerosagredos@gmail.com>
2023-11-15 17:11:35 +0100

    m

--- main.py
+++ main.py
@@ -148,4 +148,4 @@ def getEmails():
         logging.error(traceback.format_exc())
 
 if __name__ == "__main__":
-   app.run(debug=True,host='0.0.0.0',port=int(os.environ.get('PORT', 8000)))
+   app.run(debug=True,host='0.0.0.0',port=int(os.environ.get('PORT', 5001)))
